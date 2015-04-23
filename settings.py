367d4e80b72b1f5c1edb2406de718b379665ba06
Mladen <mladenpn@yahoo.com>
2015-04-23 12:14:39 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -12,5 +12,5 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = '521353195109-cpn79npk77p1u82h2os71kugvio9jr09.apps.googleusercontent.com'
+WEB_CLIENT_ID = 'WEB_CLIENT_ID'
 
